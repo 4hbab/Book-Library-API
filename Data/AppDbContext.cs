dba728ec6ce93a6896ab145479d32a72650e1e06
Sakif Ahbab <sakifahbab2@iut-dhaka.edu>
2024-05-23 11:07:40 +0600

    Initial Commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using BookLibraryAPI.Models;
+
+namespace BookLibraryAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
